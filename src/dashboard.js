e226e10f68664a86d7d3aa9df1dde43300bde7b3
Parker Helland <hellandp@gmail.com>
2019-03-18 11:45:43 -0700

    fetch API working

--- src/dashboard.js
+++ src/dashboard.js
@@ -2,3 +2,12 @@ import loadUserProfile from './templates/user-profile.js';
 
 
 loadUserProfile();
+
+const url = 'https://gateway.marvel.com/v1/public/characters?nameStartsWith=dark&apikey=23d38bd86abd4d9b4c8a0605bf740b2a';
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const characters = data.data.results;
+        console.log(characters);
+    });
