78e7648f443674baa0e7759c311105ada4e96d01
Parker Helland <hellandp@gmail.com>
2019-03-18 12:01:13 -0700

    hardcoded some hero's in html, then styled

--- src/dashboard.js
+++ src/dashboard.js
@@ -3,9 +3,9 @@ import loadUserProfile from './templates/user-profile.js';
 
 loadUserProfile();
 
-const url = 'https://gateway.marvel.com/v1/public/characters?nameStartsWith=dark&apikey=23d38bd86abd4d9b4c8a0605bf740b2a';
+const url = 'https://gateway.marvel.com/v1/public/characters?nameStartsWith=squirrel&apikey=23d38bd86abd4d9b4c8a0605bf740b2a';
 
-fetchAPI();
+// fetchAPI();
 
 function fetchAPI() {
     fetch(url)
