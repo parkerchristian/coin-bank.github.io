ef9debceb26d541d742a44834c2bb21027f7c709
Parker Helland <hellandp@gmail.com>
2019-03-18 11:47:03 -0700

    put fetch into a function to comment in/out

--- src/dashboard.js
+++ src/dashboard.js
@@ -5,9 +5,13 @@ loadUserProfile();
 
 const url = 'https://gateway.marvel.com/v1/public/characters?nameStartsWith=dark&apikey=23d38bd86abd4d9b4c8a0605bf740b2a';
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const characters = data.data.results;
-        console.log(characters);
-    });
+fetchAPI();
+
+function fetchAPI() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const characters = data.data.results;
+            console.log(characters);
+        });
+}
