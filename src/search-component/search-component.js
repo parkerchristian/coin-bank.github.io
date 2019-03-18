2e53411e82f0a577127649733d0efb4ac17136fa
Parker Helland <hellandp@gmail.com>
2019-03-18 14:27:33 -0700

    made search input to a form

--- src/search-component/search-component.js
+++ src/search-component/search-component.js
@@ -1,8 +1,13 @@
-const searchContainer = document.getElementById('search-container');
+import { writeSearchToQuery } from '../query/query-component.js';
+
+const searchForm = document.getElementById('search-form');
 const submitButton = document.getElementById('submit-button');
 const textInput = document.getElementById('text-input');
 
-submitButton.addEventListener('click', () => {
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
     const value = textInput.value;
-    console.log(value);
+    const existingQuery = window.location.hash.slice(1);
+    const newQuery = writeSearchToQuery(existingQuery, value);
+    window.location.hash = newQuery;
 });
