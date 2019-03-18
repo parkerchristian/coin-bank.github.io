8f2bb9a0f495e4317de7f6a7c360b18c52f02caf
Parker Helland <hellandp@gmail.com>
2019-03-18 14:52:15 -0700

    moved fetch into hash change event listener

--- src/dashboard.js
+++ src/dashboard.js
@@ -2,20 +2,21 @@ import loadUserProfile from './templates/user-profile.js';
 import loadCharacterCards from './templates/character-card.js';
 import './search-component/search-component.js';
 import { readFromQuery } from './query/query-component.js';
+import { makeCharacterUrl } from './api-url/api-url.js';
 
 loadUserProfile();
 
 window.addEventListener('hashchange', () => {
     const existingQuery = window.location.hash.slice(1);
     const queryOptions = readFromQuery(existingQuery);
-    
+    const url = makeCharacterUrl(queryOptions);
+    // fetchAPI(url);
 });
 
-const url = 'https://gateway.marvel.com/v1/public/characters?nameStartsWith=squirrel&apikey=23d38bd86abd4d9b4c8a0605bf740b2a';
 
-// fetchAPI();
 
-function fetchAPI() {
+
+function fetchAPI(url) {
     fetch(url)
         .then(response => response.json())
         .then(data => {
